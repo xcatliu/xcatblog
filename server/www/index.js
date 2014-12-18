8b564756a079c3f2e5c6dbafedcca497918df5e3
xcatliu <xcatliu@gmail.com>
2014-12-18 19:18:44 +0800

    rm js

--- server/www/index.js
+++ server/www/index.js
@@ -1,7 +1,9 @@
 // Enable require jsx directly
-require('node-jsx').install()
+require('node-jsx').install({extension: '.jsx'})
 
+var fs = require('fs');
 var koa = require('koa');
+var gzip = require('koa-gzip');
 var route = require('koa-route');
 
 /**
@@ -10,12 +12,14 @@ var route = require('koa-route');
  * more information, http://facebook.github.io/react/
  */
 var React = require('react');
-var ThemeEntrance = require('../../theme');
+var ThemeEntrance = require('../../theme/index.jsx');
 
 module.exports = function() {
   var context = this;
   var config = this.config;
   var app = koa();
+
+  app.use(gzip());
 
   app.use(route.get('/', function *() {
     this.body = reactRender.call(context, 'index');
@@ -27,6 +31,21 @@ module.exports = function() {
 
   app.use(route.get('/posts/:id.html', function *(id) {
     this.body = reactRender.call(context, id);
+  }));
+
+  var bundleContent = '';
+  require('./getBundle')(function(data) {
+    bundleContent = data;
+  });
+  app.use(route.get('/index.js', function *() {
+    this.type = 'application/javascript';
+    this.body = bundleContent;
+  }));
+
+  var cssContent = fs.readFileSync('theme/index.css');
+  app.use(route.get('/index.css', function *() {
+    this.type = 'text/css';
+    this.body = cssContent;
   }));
 
   app.listen(config.port || 8000);
