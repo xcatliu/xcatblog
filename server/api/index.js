193a0f5482975c42ebb6ac5ceafbea200dad4b36
xcatliu <xcatliu@gmail.com>
2014-12-29 20:00:27 +0800

    add comment

--- server/api/index.js
+++ server/api/index.js
@@ -8,17 +8,20 @@ module.exports = function() {
   var config = this.config;
   var app = koa();
 
+  // root
   app.use(route.get('/', function *() {
     // https://lodash.com/docs#omit
     this.body = _.omit(context.db, 'posts');
   }));
 
+  // posts
   app.use(route.get('/posts', function *() {
     this.body = context.db.posts.map(function(post) {
       return _.omit(post, 'contentHTML');
     });
   }));
 
+  // a post
   app.use(route.get('/posts/:id', function *(id) {
     this.body = context.db.posts.find(function(post) {
       return post.id === id;
