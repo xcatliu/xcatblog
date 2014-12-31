1d3aeae361e720bc060416ec1e64e4c2598d6bea
xcatliu <xcatliu@gmail.com>
2014-12-31 20:45:26 +0800

    update meta

--- theme/partial/head.jsx
+++ theme/partial/head.jsx
@@ -7,6 +7,7 @@ module.exports = React.createClass({
         <title>Xcat Liu</title>
         <link rel="icon" href="http://img.xcatliu.com/avatar/xcatliu_32.png" />
         <link rel="stylesheet" href="/index.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
     );
   }
